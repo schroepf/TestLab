5fcfa325bfda1ddb11db2ae8a34e75313cef46ce
Tobias Schröpf <tobias.schroepf@jambit.com>
2017-10-25 01:59:34 +0200

    Treat tests with failing assumptions as skipped

--- android/sampleapp/src/androidTest/java/de/schroepf/demoapp/MainActivityTest.java
+++ android/sampleapp/src/androidTest/java/de/schroepf/demoapp/MainActivityTest.java
@@ -10,7 +10,9 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeThat;
 
 /**
  * @author Tobias Schröpf on 17.08.16.
@@ -22,6 +24,11 @@ public class MainActivityTest {
     @Ignore
     public void testHelloWorld() throws Exception {
         onView(withId(R.id.hello_world_text_view)).check(matches(withText("This is a failing test")));
+    }
+
+    @Test
+    public void testWithFailingAssumption() throws Exception {
+        assumeThat("This is a failing assumption", 0, is(1));
     }
 
     @Test
